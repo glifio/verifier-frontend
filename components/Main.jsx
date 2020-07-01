b0a73ed8f23300be12fcd282ec8f23b59da947ac
Alex Singh <alex@alexsingh.com>
2020-07-01 16:45:23 -0300

    Update style and struture

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import PreAuth from './PreAuth'
 import PostAuth from './PostAuth'
-import { Box, Card } from './Shared'
+import {
+  Box,
+  Card,
+  IconGlif,
+  Title,
+  Text,
+  StyledATag,
+  Menu,
+  MenuItem
+} from './Shared'
 import CheckVerifiedStorageAmount from './CheckVerifiedStorageAmount'
 import { useJwt } from '../lib/JwtHandler'
 
@@ -9,54 +18,77 @@ export default () => {
   const { jwt } = useJwt()
   return (
     <>
-      <Box
-        justifyContent='center'
-        height='100vh'
-        display='flex'
-        flexGrow='1'
-        flexDirection='column'
-        alignItems='center'
-        p={3}
-      >
-        <Card
-          p={3}
-          border={0}
-          borderRadius={2}
-          borderWidth={1}
-          overflow='hidden'
+      <Box p={[3, 5]} minHeight='85vh'>
+        <Box
           display='flex'
           flexDirection='column'
-          justifyContent='space-between'
-          maxWidth='380px'
-          maxHeight='380px'
-          width='380px'
-          height='380px'
-          minHeight='380px'
-          bg='background.screen'
-          boxShadow={2}
+          alignItems='flex-start'
+          alignContent='center'
+          mb={4}
         >
-          {jwt ? <PostAuth /> : <PreAuth />}
-        </Card>
-        <Box height={5} />
-        <Card
-          width='100%'
-          p={3}
-          border={1}
-          borderRadius={2}
-          borderWidth={1}
-          overflow='hidden'
-          display='flex'
-          flexDirection='row'
-          flexWrap='wrap'
-          justifyContent='center'
-          maxWidth='380px'
-          maxHeight='380px'
-          minHeight='380px'
-          width='380px'
-          height='380px'
-        >
-          <CheckVerifiedStorageAmount />
-        </Card>
+          <Menu display='flex' justifyContent='space-between' width='100%'>
+            <MenuItem>
+              <Box
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+                borderRadius={3}
+                bg='core.primary'
+                borderRadius={3}
+                height={7}
+              >
+                <IconGlif width='48px' height='48px' fill='white' />
+              </Box>
+            </MenuItem>
+            <MenuItem>
+              <StyledATag href='#help' pb={1}>
+                Help
+              </StyledATag>
+            </MenuItem>
+          </Menu>
+          <Box display='flex' flexDirection='column' mt={4}>
+            <Title fontSize={5}>Verify your Filecoin storage</Title>
+            <Text mt={1} fontSize={4}>
+              Verified storage is cheaper for you to store and maintain.
+            </Text>
+          </Box>
+        </Box>
+        <Box display='flex' flexDirection='row' flexWrap='wrap'>
+          <Card
+            p={3}
+            my={3}
+            border={0}
+            borderRadius={2}
+            borderWidth={1}
+            overflow='hidden'
+            display='flex'
+            flexDirection='column'
+            justifyContent='space-between'
+            maxWidth='400px'
+            minWidth='320px'
+            maxHeight='320px'
+            width='100%'
+            bg='background.screen'
+            boxShadow={2}
+          >
+            {jwt ? <PostAuth /> : <PreAuth />}
+          </Card>
+          <Card
+            flexGrow='1'
+            p={3}
+            my={[0, 2, 3]}
+            border={0}
+            overflow='hidden'
+            display='flex'
+            flexDirection='row'
+            flexWrap='wrap'
+            justifyContent='center'
+            minHeight='380px'
+            minWidth='360px'
+          >
+            <CheckVerifiedStorageAmount />
+          </Card>
+        </Box>
       </Box>
     </>
   )
