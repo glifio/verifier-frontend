13cde89c46a235673bff4ff09e7ae8660d88d1f6
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-08-03 13:22:48 -0400

    Fix typo

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = (phase) => {
     return {
       env: {
         GITHUB_CLIENT_ID: '8861de8f921b556a4a0e',
-        GITHUB_REDIRECT_URL: 'https://oauth.glif.io/callback',
+        GITHUB_REDIRECT_URL: 'https://github-oauth.glif.io/callback',
         VERIFIER_URL: 'https://verifier.glif.io',
         OAUTH_STATE_STRING: 'verifier',
         LOTUS_NODE_JSONRPC: 'https://node.glif.io/02/rpc/v0',
