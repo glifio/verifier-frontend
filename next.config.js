f7e94101ef50cd1008a62f7f8bd3648974a56882
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-08-03 13:20:52 -0400

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
-const { PHASE_PRODUCTION_SERVER } = require('next/constants')
+const {
+  PHASE_PRODUCTION_BUILD,
+  PHASE_PRODUCTION_SERVER
+} = require('next/constants')
 
 module.exports = (phase) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
+  if (phase === PHASE_PRODUCTION_SERVER || phase === PHASE_PRODUCTION_BUILD) {
     return {
       env: {
         GITHUB_CLIENT_ID: '8861de8f921b556a4a0e',
-        GITHUB_REDIRECT_URL: 'https://www.verify.glif.io/callback',
+        GITHUB_REDIRECT_URL: 'https://oauth.glif.io/callback',
         VERIFIER_URL: 'https://verifier.glif.io',
-        OAUTH_STATE_STRING: 'aslkdjflskjdflksjfdlj',
+        OAUTH_STATE_STRING: 'verifier',
         LOTUS_NODE_JSONRPC: 'https://node.glif.io/02/rpc/v0',
         IS_PROD: true
       }
@@ -19,7 +22,7 @@ module.exports = (phase) => {
       GITHUB_CLIENT_ID: '82c4ac1b64e9ef7a0efa',
       GITHUB_REDIRECT_URL: 'http://localhost:3000/callback',
       VERIFIER_URL: 'http://localhost:8080',
-      OAUTH_STATE_STRING: 'aslkdjflskjdflksjfdlj',
+      OAUTH_STATE_STRING: 'verifier',
       LOTUS_NODE_JSONRPC: 'https://node.glif.io/02/rpc/v0',
       IS_PROD: false
     }
