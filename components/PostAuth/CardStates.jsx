eb518fdf51b95492fdc6ec12d65874fab851dbe5
Alex Singh <alex@alexsingh.com>
2020-08-18 16:49:50 -0300

    updated to match Faucet

--- components/PostAuth/CardStates.jsx
+++ components/PostAuth/CardStates.jsx
@@ -6,22 +6,13 @@ import { ADDRESS_PROPTYPE } from '../../customPropTypes'
 export const Confirming = ({ err }) => {
   return (
     <>
-      <Text>
-        We're making sure your verification gets processed by the Filecoin
-        network...
-      </Text>
-      <Box
-        display='flex'
-        flexDirection='column'
-        justifyContent='flex-start'
-        width='100%'
-      >
+      
         {err && (
-          <Label color='status.fail.background' mt={3} mb={0}>
+          <Label color='status.fail.background' m={0}>
             {err}
           </Label>
         )}
-      </Box>
+
     </>
   )
 }
@@ -37,20 +28,20 @@ Confirming.defaultProps = {
 export const Confirmed = ({ address, cid }) => {
   return (
     <>
-      <Text>
-        All set!{' '}
-        <Text display='inline' color='core.primary'>
-          {address}
-        </Text>{' '}
-        has an 8GB verified Filecoin allowance.
-      </Text>
+
+      <Text display="inline" m={0}>
+        All set! {address} has an 8GB verified Filecoin allowance.
       <StyledATag
+        display="inline"
         rel='noopener noreferrer'
         target='_blank'
         href={`https://filscan.io/#/message/detail?cid=${cid}`}
       >
-        <Label color='core,primary'>View transaction on Filscan</Label>
+        <Label display="inline" mx={2} color='core.primary'>VIEW</Label>
       </StyledATag>
+      </Text>
+
+
     </>
   )
 }
