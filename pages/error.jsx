6900e19af09892f2ed953ff4c8560dc4f24da639
Thijs van Hoof <thijs.v.h@gmail.com>
2022-03-03 14:27:21 +0100

    Linter fixes after react named export fix

--- pages/error.jsx
+++ pages/error.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import ErrorView from '../components/ErrorView'
 
-const Error = () => <ErrorView
-  title='Oops, something went wrong.'
-  description="We've been notified of the issue."
-  linkDisplay='Follow @glifio for updates.'
-  linkhref='https://twitter.com/glifio'
-/>;
+const Error = () => (
+  <ErrorView
+    title='Oops, something went wrong.'
+    description="We've been notified of the issue."
+    linkDisplay='Follow @glifio for updates.'
+    linkhref='https://twitter.com/glifio'
+  />
+)
 
-export default Error;
+export default Error
