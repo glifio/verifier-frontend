ba29499a7d2fd830aea9a9be4317c51ed0f690e8
Thijs van Hoof <thijs.v.h@gmail.com>
2022-03-03 14:22:32 +0100

    export Error as named component

--- pages/error.jsx
+++ pages/error.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 import ErrorView from '../components/ErrorView'
 
-export default () => (
-  <ErrorView
-    title='Oops, something went wrong.'
-    description="We've been notified of the issue."
-    linkDisplay='Follow @glifio for updates.'
-    linkhref='https://twitter.com/glifio'
-  />
-)
+const Error = () => <ErrorView
+  title='Oops, something went wrong.'
+  description="We've been notified of the issue."
+  linkDisplay='Follow @glifio for updates.'
+  linkhref='https://twitter.com/glifio'
+/>;
+
+export default Error;
