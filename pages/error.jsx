c03bb124cafb6c69d814f8a750f6852336746d02
Thijs van Hoof <thijs.v.h@gmail.com>
2022-03-07 23:18:41 +0100

    Style error page and use ErrorView from react-components

--- pages/error.jsx
+++ pages/error.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-
-import ErrorView from '../components/ErrorView'
+import { ErrorView, OneColumnCentered } from '@glif/react-components'
+import VerifierPage from '../components/VerifierPage'
 
 const Error = () => (
-  <ErrorView
-    title='Oops, something went wrong.'
-    description="We've been notified of the issue."
-    linkDisplay='Follow @glifio for updates.'
-    linkhref='https://twitter.com/glifio'
-  />
+  <VerifierPage>
+    <OneColumnCentered>
+      <ErrorView
+        title='Oops, something went wrong.'
+        description="We've been notified of the issue."
+        linkDisplay='Follow @glifio for updates.'
+        linkhref='https://twitter.com/glifio'
+      />
+    </OneColumnCentered>
+  </VerifierPage>
 )
 
 export default Error
